a57254c45a36b0dfbb073a6c4a14ea620a65760d
Carolina Arvidsson <carolinaarvidsson_@live.se>
2020-10-12 23:26:55 +0200

    just basic stuff

---
+++ src/components/WrapperComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class WrapperComponent extends Component {
+    constructor (props) {
+        super(props);
+    };
+    render () {
+        return (
+            <div className='WrapperComponent'></div>
+        );
+    };
+};
+
+export default WrapperComponent;
