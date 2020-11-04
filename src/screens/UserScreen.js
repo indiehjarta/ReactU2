fa08b22daa97bc00cf9c8d2b925c663a796803a5
Carolina Arvidsson <carolinaarvidsson_@live.se>
2020-11-04 15:06:41 +0100

    added comps and screens

---
+++ src/screens/UserScreen.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class UserScreen extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+};
+
+export default UserScreen;
